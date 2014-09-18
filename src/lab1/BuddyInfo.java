93b9cf66e65d76b2a826918c1b0ec8d0279605ca
adhirajchakraborty <adhirajchakraborty@me4324-06.labs.sce.carleton.ca>
2014-09-18 16:14:01 -0400

    This is an edited version

---
+++ src/lab1/BuddyInfo.java
@@ -0,0 +1,47 @@
+
+package lab1;
+
+
+
+public class BuddyInfo {
+	private String name;
+	private String address;
+	private String phone_number;
+	public BuddyInfo(String name, String address, String phone_number){
+		super();
+	
+	}
+	public static void main(String[] args) {
+	
+		// TODO Auto-generated method stub
+	}
+
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return name;
+	}
+	
+
+	public String getAddress() {
+		return address;
+	}
+
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+
+	public String getPhone_number() {
+		return phone_number;
+	}
+
+
+	public void setPhone_number(String phone_number) {
+		this.phone_number = phone_number;
+	}
+
+
+	
+}
