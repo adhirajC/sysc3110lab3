8d468ee06fee95e1afb9e2f437dbdfa35efd9f18
adhirajchakraborty <adhirajchakraborty@me4324-06.labs.sce.carleton.ca>
2014-09-18 16:01:10 -0400

    hello everyone

---
+++ src/lab1/AddressBoook.java
@@ -0,0 +1,36 @@
+package lab1;
+
+import java.util.ArrayList;
+
+
+
+public class AddressBoook{
+	private  ArrayList<BuddyInfo> AddressBoook = new ArrayList<BuddyInfo>();
+		
+ 
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+BuddyInfo buddy= new BuddyInfo("adhi","tonrot","838");
+AddressBoook stuff= new AddressBoook();
+System.out.println("hello!!");
+
+stuff.addBuddy(buddy);
+stuff.removeBuddy(0);
+	}
+
+public void removeBuddy(int index){
+AddressBoook.remove(index);
+}
+
+
+public void addBuddy(BuddyInfo abuddy){
+AddressBoook.add(abuddy);
+
+}
+}
+
+
